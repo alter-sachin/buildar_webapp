eaadd6184e9d4d1b06013af53f8c8adf95f37337
Sachin Sharma <sachin.sharma@jungleworks.com>
2021-01-13 16:26:08 +0530

    created navbar homepage component

---
+++ client/common/media/icons/Mainlogo.js
@@ -0,0 +1,11 @@
+import React from "react";
+import mainLogo from'./mainLogo.png';
+
+
+const Mainlogo = props => {
+	return (
+		<img  src={mainLogo} alt="BuildAR.in" style={{width:"10%",height:"10%"}}/>
+	);
+};
+
+export default Mainlogo;
--- client/modules/homepage/components/Navbar.js
+++ client/modules/homepage/components/Navbar.js
@@ -2,21 +2,54 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { t } from "shared/translations/i18n";
+import Mainlogo from "common/media/icons/Mainlogo"
 
-import SuccessNotification from "common/components/SuccessNotification";
-import ServerSuccess from "common/components/ServerSuccess";
-import ServerError from "common/components/ServerError";
-import InputField from "common/components/inputs/InputField";
-import Checkbox from "common/components/inputs/Checkbox";
 
 class Navbar extends Component {
 	render() {
 		return (
-			<div>
-				<div className="w-100 mb-3">
-					<span className="h3">Hello World</span>
-				</div>
-			</div>
+			<nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+        <div className="container">
+          <a className="navbar-brand logo-image" href="index.html"><img src="images/mainLogo.png" alt="alternative" style={{height:"10%",width:"10%"}}/></a> 
+          {/* Mobile Menu Toggle Button */}
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-awesome fas fa-bars" />
+            <span className="navbar-toggler-awesome fas fa-times" />
+          </button>
+          {/* end of mobile menu toggle button */}
+          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link page-scroll" href="#header">HOME <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link page-scroll" href="#features">FEATURES</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link page-scroll" href="#details">DETAILS</a>
+              </li>
+              {/* Dropdown Menu */}          
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle page-scroll" href="#video" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">VIDEO</a>
+                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <a className="dropdown-item" href="article-details.html"><span className="item-text">ARTICLE DETAILS</span></a>
+                  <div className="dropdown-items-divide-hr" />
+                  <a className="dropdown-item" href="terms-conditions.html"><span className="item-text">TERMS CONDITIONS</span></a>
+                  <div className="dropdown-items-divide-hr" />
+                  <a className="dropdown-item" href="privacy-policy.html"><span className="item-text">PRIVACY POLICY</span></a>
+                </div>
+              </li>
+              {/* end of dropdown menu */}
+              <li className="nav-item">
+                <a className="nav-link page-scroll" href="#pricing">PRICING</a>
+              </li>
+            </ul>
+            <span className="nav-item">
+              <a className="btn-outline-sm" href="log-in.html">LOG IN</a>
+            </span>
+          </div>
+        </div> {/* end of container */}
+      </nav>
 		);
 	}
 }
