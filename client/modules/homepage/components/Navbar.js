289ca42ebf356b1e1bfcd97a613d35f3a8357445
Nishant-Pall <palln6935@gmail.com>
2021-01-18 13:31:52 +0530

    Fixed LOGIN button to fit the text

--- client/modules/homepage/components/Navbar.js
+++ client/modules/homepage/components/Navbar.js
@@ -6,54 +6,54 @@ import Mainlogo from "common/media/icons/Mainlogo"
 
 
 class Navbar extends Component {
-	render() {
-		return (
+  render() {
+    return (
       <div>
-			<nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top" style={{backgroundColor:"#726bfa"}}>
-        <div className="container">
-          <a className="navbar-brand logo-image" href="index.html"><img src="images/mainLogo.png" alt="alternative" style={{height:"10%",width:"10%"}}/></a> 
-          {/* Mobile Menu Toggle Button */}
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-awesome fas fa-bars" />
-            <span className="navbar-toggler-awesome fas fa-times" />
-          </button>
-          {/* end of mobile menu toggle button */}
-          <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link page-scroll" href="#header">HOME <span className="sr-only">(current)</span></a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link page-scroll" href="#features">FEATURES</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link page-scroll" href="#details">DETAILS</a>
-              </li>
-              {/* Dropdown Menu */}          
-              <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle page-scroll" href="#video" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">VIDEO</a>
-                <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <a className="dropdown-item" href="article-details.html"><span className="item-text">ARTICLE DETAILS</span></a>
-                  <div className="dropdown-items-divide-hr" />
-                  <a className="dropdown-item" href="terms-conditions.html"><span className="item-text">TERMS CONDITIONS</span></a>
-                  <div className="dropdown-items-divide-hr" />
-                  <a className="dropdown-item" href="privacy-policy.html"><span className="item-text">PRIVACY POLICY</span></a>
-                </div>
-              </li>
-              {/* end of dropdown menu */}
-              <li className="nav-item">
-                <a className="nav-link page-scroll" href="#pricing">PRICING</a>
-              </li>
-            </ul>
-            <span className="nav-item">
-				    <Link to="/signin" className="btn-outline-sm">LOG IN</Link>
-            </span>
-          </div>
-        </div> {/* end of container */}
-      </nav>
+        <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top" style={{ backgroundColor: "#726bfa" }}>
+          <div className="container">
+            <a className="navbar-brand logo-image" href="index.html"><img src="images/mainLogo.png" alt="alternative" style={{ height: "10%", width: "10%" }} /></a>
+            {/* Mobile Menu Toggle Button */}
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-awesome fas fa-bars" />
+              <span className="navbar-toggler-awesome fas fa-times" />
+            </button>
+            {/* end of mobile menu toggle button */}
+            <div className="collapse navbar-collapse" id="navbarsExampleDefault">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                  <a className="nav-link page-scroll" href="#header">HOME <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link page-scroll" href="#features">FEATURES</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link page-scroll" href="#details">DETAILS</a>
+                </li>
+                {/* Dropdown Menu */}
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle page-scroll" href="#video" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">VIDEO</a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="article-details.html"><span className="item-text">ARTICLE DETAILS</span></a>
+                    <div className="dropdown-items-divide-hr" />
+                    <a className="dropdown-item" href="terms-conditions.html"><span className="item-text">TERMS CONDITIONS</span></a>
+                    <div className="dropdown-items-divide-hr" />
+                    <a className="dropdown-item" href="privacy-policy.html"><span className="item-text">PRIVACY POLICY</span></a>
+                  </div>
+                </li>
+                {/* end of dropdown menu */}
+                <li className="nav-item">
+                  <a className="nav-link page-scroll" href="#pricing">PRICING</a>
+                </li>
+              </ul>
+              <span className="nav-item">
+                <Link to="/signin" className="btn-outline-sm">LOGIN</Link>
+              </span>
+            </div>
+          </div> {/* end of container */}
+        </nav>
       </div>
-		);
-	}
+    );
+  }
 }
 
 
