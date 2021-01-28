385095d4198b53e349f280fa4582a1e55a15ddfd
Mathew <57001761+mathewpius19@users.noreply.github.com>
2021-01-28 16:50:35 +0530

    Added video file

---
+++ server/controller/video.js
@@ -0,0 +1,44 @@
+import restrict from "utilities/restrictRoutes";
+import browserResponseLng from "utilities/browserResponseLng";
+import { ServerResponseError } from "utilities/errors/serverResponseError";
+
+import { variableExists, keyNameCorrect, removeUniqueProperties } from "shared/utilities/filters";
+import validate from "shared/validation/validate";
+import { t } from "shared/translations/i18n";
+import { updateUserProfile, verifyEmail, changeSavedLanguage, changeUserPassword } from "shared/validation/profile";
+
+import {
+	loadVideo
+} from "../orchestrator/video";
+
+module.exports= function(router){
+	router.get("/api/v1.0/video",
+		restrict({
+			registered:false,
+			unregistered:true
+		}),
+		function(req,res,next){
+            
+			// console.log(req)
+			// const browserLng = browserResponseLng(req);
+
+			const authenticatedUser = {
+				userId:1,
+				
+			};
+
+			loadVideo(null, authenticatedUser, null)
+				.then(
+					result=>{
+						
+						 console.log(result)
+							return res.status(200).send(result);
+						
+					},
+					error =>{
+						return next(error);
+					}
+            	);
+			// return res.status(200).send("success");
+		});
+};
