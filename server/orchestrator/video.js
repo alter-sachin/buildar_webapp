d929d8f5c37cafec730060f33144f46b82bd4f4f
Nishant-Pall <palln6935@gmail.com>
2021-01-29 20:24:37 +0530

    create and export deleteVideo function

--- server/orchestrator/video.js
+++ server/orchestrator/video.js
@@ -9,40 +9,65 @@ import { t } from "shared/translations/i18n";
 import { EMAIL_TYPE, LANGUAGE_CODES, SIGNED_URL_EXPIRY_TIME, ACL_POLICIES } from "shared/constants";
 import { ServerResponseError } from "utilities/errors/serverResponseError";
 
-export function loadVideo(requestProperties, authenticatedUser, browserLng){
-	return database().transaction(async function(transaction){
+export function loadVideo(requestProperties, authenticatedUser, browserLng) {
+	return database().transaction(async function (transaction) {
 		try {
 			// Load client for authenticated user
-			const user = await models().user.findOne({ where: { id: authenticatedUser.userId,  active: true } });
+			const user = await models().user.findOne({ where: { id: authenticatedUser.userId, active: true } });
 			//  console.log(user);	
-			const video = await models().video.findAll({where:{userId_FK:user.id}});
+			const video = await models().video.findAll({ where: { userId_FK: user.id } });
 			return video;
 		}
-		catch(error){
+		catch (error) {
 			console.error(error);
 		}
-		
+
 	});
 }
 
-export function createVideo(requestProperties, authenticatedUser, browserLng){
-	return database().transaction(async function(transaction){
-		try{
+export function createVideo(requestProperties, authenticatedUser, browserLng) {
+	return database().transaction(async function (transaction) {
+		try {
 			//Create video for authenticated User
 			// const user = await models().user.findOne({where :{id:authenticatedUser.userId, active:true}});
 			console.log(requestProperties);
-			const videoObj={
-				title:requestProperties.title,
-				description:requestProperties.description,
-				thumbnail:requestProperties.thumbnail,
-				textScript:requestProperties.textScript,
+			const videoObj = {
+				title: requestProperties.title,
+				description: requestProperties.description,
+				thumbnail: requestProperties.thumbnail,
+				textScript: requestProperties.textScript,
 				videoURL: requestProperties.videoUrl,
-				userId_FK:requestProperties.userId
+				userId_FK: requestProperties.userId
 			}
-			const videoInstance = await models().video.create(videoObj, {transaction:transaction});
+			const videoInstance = await models().video.create(videoObj, { transaction: transaction });
 			return videoInstance;
 		}
-		catch(error){
+		catch (error) {
+			console.error(error);
+			return error;
+		}
+	});
+}
+
+
+export function deleteVideo(requestProperties, authenticatedUser, browserLng) {
+	return database().transaction(async function (transaction) {
+		try {
+			//Create video for authenticated User
+			// const user = await models().user.findOne({where :{id:authenticatedUser.userId, active:true}});
+			console.log(requestProperties);
+			const videoObj = {
+				// title: requestProperties.title,
+				// description: requestProperties.description,
+				// thumbnail: requestProperties.thumbnail,
+				// textScript: requestProperties.textScript,
+				// videoURL: requestProperties.videoUrl,
+				userId_FK: requestProperties.userId
+			}
+			const videoInstance = await models().video.destroy({ where: { userId_FK: user.id } }, { transaction: transaction });
+			return videoInstance;
+		}
+		catch (error) {
 			console.error(error);
 			return error;
 		}
