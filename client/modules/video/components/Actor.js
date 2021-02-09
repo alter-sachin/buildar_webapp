24c7a88060a5855cb46273192e656cc0b5b1b962
Mathew <57001761+mathewpius19@users.noreply.github.com>
2021-02-09 13:41:10 +0530

    reverted actor.js and index.js in video module

--- client/modules/video/components/Actor.js
+++ client/modules/video/components/Actor.js
@@ -12,7 +12,6 @@ class Actor extends Component {
 		this.audioRequest = this.audioRequest.bind(this);
 		this.handleCheck = this.handleCheck.bind(this);
 	}
-<<<<<<< HEAD
         state = {
         	actors: this.props.data.actors,
         	active: 0,
@@ -91,82 +90,6 @@ class Actor extends Component {
 					 onClick = {this.audioRequest}>
 					
                     Listen
-=======
-	state = {
-		actors: this.props.data,
-		active: 0,
-		textVal: "",
-		audioUrl: "",
-		isClicked: false
-	}
-
-
-	handleChange() {
-		this.setState({ textVal: event.target.value });
-	}
-	audioRequest() {
-		const data = {
-			textScript: this.state.textVal,
-			speakerId: 0,
-			speed: 0
-		};
-		axios.post(
-			"api/v1.0/audio", data)
-			.then(({ data }) => {
-				console.log(data);
-				this.setState({
-					audioUrl: data.audioUrl,
-					isClicked: true
-				});
-			});
-	}
-	renderTime = ({ remainingTime }) => {
-		if (remainingTime === 0) {
-			return <div className="timer">Done!!</div>;
-		}
-
-		return (
-			<div className="timer">
-				{/* <div className="text">Remaining</div> */}
-				<div className="value">{remainingTime}</div>
-				{/* <div className="text">seconds</div> */}
-			</div>
-		);
-	};
-
-	render() {
-		console.log(this.state.audioUrl);
-		const { actors } = this.state;
-
-
-
-		return (
-			<div>
-				<h4>Select Actor</h4>
-				<div className="actor-list">
-					{actors.map((actor) => (
-						<p className="actor" key={actor.name}>
-							<img src={actor.thumbnail} style={{ height: "100px" }} />
-							{actor.name}
-						</p>
-					))}
-				</div>
-				<div className="script-input">
-					<h4>Script</h4>
-					<textarea
-						className="text-area"
-						placeholder="Insert script here"
-						rows="4"
-						cols="50"
-						value={this.state.textVal}
-						onChange={this.handleChange}
-					/>
-				</div>
-				<div className="audio-listen">
-					<button type="button" className="btn btn-secondary listen-button"
-						onClick={this.audioRequest}>
-						Listen
->>>>>>> origin/ux
         			</button>
 					<audio className="audio-player" controls>
 						{this.state.audioUrl === "" ? "Nothing to play yet" : <source src={this.state.audioUrl} type="audio/wav" />}
