5ca7ed879a26ee11cd2ca4c3dd31514b8d1a9c03
nishant <palln6935@gmail.com>
2021-02-16 14:58:59 +0530

    fix styles in navbar and header files

--- client/modules/homepage/components/Navbar.js
+++ client/modules/homepage/components/Navbar.js
@@ -11,7 +11,7 @@ class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top" style={{ backgroundColor: "#726bfa" }}>
           <div className="container">
-            <a className="navbar-brand logo-image" href="index.html"><img src="images/mainLogo.png" alt="alternative" style={{ height: "10%", width: "10%" }} /></a>
+            <a className="navbar-brand logo-image" href="index.html"><img src="images/Logo_BuildAR..png" alt="alternative" style={{ height: "10%", width: "10%" }} /></a>
             {/* Mobile Menu Toggle Button */}
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-awesome fas fa-bars" />
@@ -20,33 +20,42 @@ class Navbar extends Component {
             {/* end of mobile menu toggle button */}
             <div className="collapse navbar-collapse" id="navbarsExampleDefault">
               <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link page-scroll" href="#header">HOME <span className="sr-only">(current)</span></a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link page-scroll" href="#features">FEATURES</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link page-scroll" href="#details">DETAILS</a>
-                </li>
-                {/* Dropdown Menu */}
                 <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle page-scroll" href="#video" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">VIDEO</a>
+                  <a className="nav-link dropdown-toggle page-scroll" href="#products" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">Products</a>
                   <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a className="dropdown-item" href="article-details.html"><span className="item-text">ARTICLE DETAILS</span></a>
+                    <a className="dropdown-item" href="article-details.html"><span className="item-text">Create Videos</span></a>
                     <div className="dropdown-items-divide-hr" />
-                    <a className="dropdown-item" href="terms-conditions.html"><span className="item-text">TERMS CONDITIONS</span></a>
+                    <a className="dropdown-item" href="terms-conditions.html"><span className="item-text">Personalize Videos</span></a>
                     <div className="dropdown-items-divide-hr" />
-                    <a className="dropdown-item" href="privacy-policy.html"><span className="item-text">PRIVACY POLICY</span></a>
+                    <a className="dropdown-item" href="privacy-policy.html"><span className="item-text">API</span></a>
                   </div>
                 </li>
-                {/* end of dropdown menu */}
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle page-scroll" href="#usecases" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">Use Cases</a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="article-details.html"><span className="item-text">Sales</span></a>
+                    <div className="dropdown-items-divide-hr" />
+                    <a className="dropdown-item" href="terms-conditions.html"><span className="item-text">Marketing</span></a>
+                    <div className="dropdown-items-divide-hr" />
+                    <a className="dropdown-item" href="privacy-policy.html"><span className="item-text">Chatbot</span></a>
+                  </div>
+                </li>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown-toggle page-scroll" href="#learn" id="navbarDropdown" role="button" aria-haspopup="true" aria-expanded="false">Learn</a>
+                  <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a className="dropdown-item" href="article-details.html"><span className="item-text">Case Studies</span></a>
+                    <div className="dropdown-items-divide-hr" />
+                    <a className="dropdown-item" href="terms-conditions.html"><span className="item-text">Blogs</span></a>
+                    <div className="dropdown-items-divide-hr" />
+                    <a className="dropdown-item" href="privacy-policy.html"><span className="item-text">Developer API</span></a>
+                  </div>
+                </li>
                 <li className="nav-item">
-                  <a className="nav-link page-scroll" href="#pricing">PRICING</a>
+                  <Link to="/signin" className="nav-link page-scroll" >Signin</Link>
                 </li>
               </ul>
               <span className="nav-item">
-                <Link to="/signin" className="btn-outline-sm">LOGIN</Link>
+                <Link to="/register" className="btn-outline-sm">Signup</Link>
               </span>
             </div>
           </div> {/* end of container */}
