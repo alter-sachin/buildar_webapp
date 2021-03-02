22de9a9826cbf65a3479086558e0e65875e489e3
nishant <palln6935@gmail.com>
2021-03-02 17:50:07 +0530

    Create and style dropdown for video cards

--- client/modules/myVideos/components/Videocard.js
+++ client/modules/myVideos/components/Videocard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton'
 
 class Videocard extends Component {
     constructor(props) {
@@ -14,12 +16,26 @@ class Videocard extends Component {
     render() {
         const { title, description, url } = this.props
         return (
-            <div>
+            <div className="myvideo-card-item">
                 <h2>{title}</h2>
                 <p>{description}</p>
-                <video controls width="300" height="240">
-                    <source src={url} />
-                </video>
+                <div className="myvideo-card">
+                    <div className="myvideo-card-menu">
+                        <Dropdown>
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                                <i className="fa fa-bars"></i>
+                            </Dropdown.Toggle>
+                            <Dropdown.Menu>
+                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <video className="myvideo-vid" controls width="300" height="240">
+                        <source src={url} />
+                    </video>
+                </div>
             </div>
         )
     }
