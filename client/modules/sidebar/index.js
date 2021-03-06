39d87d1173d8ba90588fd9693e6742242874ad36
alter-sachin <guitarplayersachin@gmail.com>
2021-03-06 15:58:53 +0530

    added sidebar menu items

--- client/modules/sidebar/index.js
+++ client/modules/sidebar/index.js
@@ -1,6 +1,8 @@
 // import React, { useState } from 'react';
 // import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
+import * as CgIcons from 'react-icons/cg';
+import * as FaIcons from 'react-icons/fa';
 // import { Link } from 'react-router-dom';
 // import { SidebarItem } from './sidebarItem';
 // import { IconContext } from 'react-icons';
@@ -70,9 +72,53 @@ class SideBar extends Component {
                 <div className="w3-sidebar w3-bar-block w3-card w3-animate-left" style={{ display: "none" }} id="mySidebar">
                     <button className="w3-bar-item w3-button w3-large"
                         onClick={this.w3_close}>Close &times;</button>
-                    <a href="#" className="w3-bar-item w3-button"><AiIcons.AiOutlineVideoCameraAdd />Link 1</a>
-                    <a href="#" className="w3-bar-item w3-button">Link 2</a>
-                    <a href="#" className="w3-bar-item w3-button">Link 3</a>
+                    <div className="navigation_logoSection">
+                        <div className="navigation_symbol">
+                            <span id="small_symbol"></span>
+                        </div>
+                    </div>
+                    <a href="#" className="w3-bar-item w3-button"><span id="text"><strong>Videos</strong></span></a>
+                    <a href="#" className="w3-bar-item w3-button">
+                        <span id="icon">
+                            <AiIcons.AiOutlineVideoCameraAdd/>
+                        </span>
+                        <span id="text">
+                            Create Video
+                        </span>
+                    </a>
+                    <a href="#" className="w3-bar-item w3-button">
+                        <span id="icon">
+                            <AiIcons.AiOutlineOrderedList/>
+                        </span>
+                        <span id="text">
+                            My Videos
+                        </span>
+                    </a>
+                    <div className="bottom_menu">
+                        <a href="#" className="w3-bar-item w3-button"><span id="text"><strong>Settings</strong></span></a>
+                        <a href="#" className="w3-bar-item w3-button">
+                            <span id="icon">
+                                <CgIcons.CgProfile/>
+                            </span>
+                            <span id="text">
+                                Profile
+                            </span>
+                        </a>
+                        <a href="#" className="w3-bar-item w3-button">
+                            <span id="icon">
+                                <FaIcons.FaFileInvoiceDollar/>
+                            </span>
+                            <span id="text">
+                                Billing
+                            </span>
+                        </a>
+                    </div>
+                    <div className="bottom_button">
+                        <button>
+                        <span id="inviteicon"><AiIcons.AiOutlineUsergroupAdd/></span>
+                        <span id="invite">Invite Users</span>
+                        </button>
+                    </div>
                 </div>
             </div>
         )
