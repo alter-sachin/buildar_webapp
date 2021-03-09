5d608f63f64bbdaf28ff1dc34b8f818810b50b76
nishant <palln6935@gmail.com>
2021-03-09 12:00:45 +0530

    Fix react-icons

--- client/modules/myvideos/components/Videocard.js
+++ client/modules/myvideos/components/Videocard.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
-
+import * as AiIcons from 'react-icons/ai';
+import * as CgIcons from 'react-icons/cg';
+import * as FaIcons from 'react-icons/fa';
 class Videocard extends Component {
     constructor(props) {
         super(props)
@@ -21,26 +22,26 @@ class Videocard extends Component {
                 <div className="myvideo-card">
                     <div className="myvideo-card-menu">
                         <Dropdown id="myvideo-card-menu-dropdown">
-                            <Dropdown.Toggle variant="secondary" id="dropdown-basic">
+                            <Dropdown.Toggle variant="secondary" id="dropdown-basic" size="sm">
                                 <i className="fa fa-bars"></i>
                             </Dropdown.Toggle>
                             <Dropdown.Menu>
-                                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                                <Dropdown.Item href="#/action-1">Edit</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2">Delete</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3">Duplicate</Dropdown.Item>
                             </Dropdown.Menu>
                         </Dropdown>
                     </div>
                     <a href="/video">
                         <img className="myvideo-thumbnail" src="images/Logo_BuildAR.png" />
                     </a>
-                    <p>{title}</p>
+                    <p className="myvideo-title" contentEditable="true">{title}</p>
                 </div>
                 <div className="myvideo-time">
                     <p>
                         {timeCreated}
-                        <i id="myvideo-footer-icons" className="far fa-star"></i>
-                        <i id="myvideo-footer-icons" className="fas fa-link"></i>
+                        <div id="myvideo-footer-icons"><AiIcons.AiOutlineStar /></div>
+                        <div id="myvideo-footer-icons"><AiIcons.AiOutlineLink /></div>
                     </p>
                 </div>
             </div>
