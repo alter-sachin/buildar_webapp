60b7998cadd679b4c67ca0938e0ea1b6a96ba43f
alter-sachin <guitarplayersachin@gmail.com>
2021-03-12 14:28:57 +0530

    style and position changes on actor component

--- client/modules/createvideo/components/Actor.js
+++ client/modules/createvideo/components/Actor.js
@@ -45,6 +45,7 @@ class Actor extends Component {
 		audioUrl: "",
 		videoUrl: "https://buildar.in/vid/1614581284.455946.mp4",
 		audioReceived: false,
+		videoReceived: false,
 		isAudioClicked: false,
 		isVideoClicked: false,
 		key: 0,
@@ -105,7 +106,10 @@ class Actor extends Component {
 
 	}
 	audioRequest() {
-		this.startAudioTimer()
+		if (this.state.textVal !== "") {
+			document.getElementById("audio-timer-wrapper").style.display = 'block'
+			this.startAudioTimer()
+		}
 
 		const data = {
 			speakerId: this.state.selectedElement.value,
@@ -128,11 +132,15 @@ class Actor extends Component {
 
 	handleSave() {
 		var h1Text = document.querySelector(".video-title-edit").textContent;
-        this.setState({videoTitle:h1Text});
+		this.setState({ videoTitle: h1Text });
 	}
 
 	videoRequest() {
-		this.startVideoTimer()
+
+		if (this.state.audioReceived) {
+			document.getElementById("video-timer-wrapper").style.display = 'block'
+			this.startVideoTimer()
+		}
 		const data = {
 			actorId: this.state.actorId,
 			audioUrl: this.state.audioUrl,
@@ -142,18 +150,22 @@ class Actor extends Component {
 			"http://fcbbd04a559e.ngrok.io/video", data)
 			.then(({ data }) => {
 				this.setState({
-					videoUrl: data.videoUrl
+					videoUrl: data.videoUrl,
+					videoReceived: true
 				});
 			})
+		if (this.state.videoReceived) {
+			document.getElementById("save_video").disabled = false;
+		}
 	}
 	saveVideo() {
 		const data = {
 			title: this.state.videoTitle,
-			description:"this is first video",
-			thumbnail:"",
-			textScript:this.state.textVal,
-			videoUrl:this.state.videoUrl,
-			userId_FK:this.props.user.get("userId")
+			description: "this is first video",
+			thumbnail: "",
+			textScript: this.state.textVal,
+			videoUrl: this.state.videoUrl,
+			userId_FK: this.props.user.get("userId")
 		}
 		axios.post(
 			"/api/v1.0/video", data)
@@ -162,7 +174,7 @@ class Actor extends Component {
 			})
 	}
 
-	
+
 
 	w3_open = () => {
 		document.getElementById("main").style.marginLeft = "15%";
@@ -174,7 +186,7 @@ class Actor extends Component {
 
 	renderVideoTime = ({ remainingTime }) => {
 		if ((this.state.videoUrl !== "" || this.state.videoUrl !== "https://buildar.in/vid/1614581284.455946.mp4") && this.state.isVideoClicked == false) {
-			return <div className="timer">Done!!</div>;
+			return <div className="timer">Done</div>;
 		}
 
 		if (this.state.videoUrl !== "" && this.state.isVideoClicked == true) {
@@ -191,7 +203,7 @@ class Actor extends Component {
 	};
 	renderAudioTime = ({ remainingTime }) => {
 		if (this.state.audioUrl !== "" && this.state.isAudioClicked == false) {
-			return <div className="timer">Done!!</div>;
+			return <div className="timer">Done</div>;
 		}
 
 		if (this.state.audioUrl !== "" && this.state.isAudioClicked == true) {
@@ -210,24 +222,32 @@ class Actor extends Component {
 	render() {
 		const { user, history } = this.props;
 		const { actors, voices } = this.state;
-		console.log(this.state.textVal);
-		console.log(this.state.videoTitle);
 
-		
+
 
 		return (
 			<div>
 				<button id="openNav" className="w3-button w3-xlarge" onClick={this.w3_open}>&#9776;</button>
 				<div className="topnav">
+					<a type="button" href="/myvideos" className="btn btn-link" id="cancel">Go Back</a>
 					<span id="editable">
-					<h1 className="video-title-edit" contentEditable="true"  onMouseOut={this.handleSave}  placeholder="Enter Title">Click here to enter Title</h1>
-
-					
+						<h1 className="video-title-edit" contentEditable="true" onMouseOut={this.handleSave} placeholder="Enter Title">Click here to enter Title</h1>
 					</span>
-					<div className="topnav-right">
-						<a type="button" href="/myvideos" className="btn btn-link" id="cancel">Cancel</a>
+					<div className="d-flex topnav-right">
+						<div id="video-timer-wrapper">
+							<CountdownCircleTimer
+								key={this.state.key}
+								isPlaying={this.state.isVideoClicked}
+								duration={20}
+								colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+								onComplete={() => [true, 0]}
+								strokeWidth={5}
+								size={50}>
+								{this.renderVideoTime}
+							</CountdownCircleTimer>
+						</div>
 						<button onClick={this.videoRequest} id="create_video">Create Video</button>
-						<button className="btn btn-success" onClick={this.saveVideo} id="save_video">Save Video</button>
+						<button className="btn btn-success" onClick={this.saveVideo} id="save_video" disabled>Save Video</button>
 					</div>
 				</div>
 				<div className="col-md-12 col-lg-12 left_right">
@@ -238,8 +258,8 @@ class Actor extends Component {
 								<h4>Select Actor</h4>
 								<div className="actor-list">
 									{actors.map((actor) => (
-										<p className="actor" key={actor.name} onClick={() => this.selectActor(actor.actorId, actor.gender)}>
-											<img src={actor.thumbnail} tabIndex="1" style={{ height: "100px" }} />
+										<p className="actor" id="actor-select" key={actor.name} onClick={() => this.selectActor(actor.actorId, actor.gender)}>
+											<img id="actor-image" src={actor.thumbnail} style={{ height: "100px" }} />
 											{actor.name}
 										</p>
 									))}
@@ -272,15 +292,15 @@ class Actor extends Component {
 										Listen
 	        					</button>
 									<AudioPlayer className="audio-player" audioUrl={this.state.audioUrl} />
-									<div className="timer-wrapper">
+									<div id="audio-timer-wrapper">
 										<CountdownCircleTimer
 											key={this.state.key}
 											isPlaying={this.state.isAudioClicked}
 											duration={10}
 											colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
 											onComplete={() => [true, 0]}
-											strokeWidth={8}
-											size={75}>
+											strokeWidth={6}
+											size={55}>
 											{this.renderAudioTime}
 										</CountdownCircleTimer>
 									</div>
@@ -288,18 +308,6 @@ class Actor extends Component {
 							</div>
 							<div className="col-md-6 col-lg-6 right_side">
 								<Videoplayer videoUrl={this.state.videoUrl} />
-								<div className="timer-wrapper">
-									<CountdownCircleTimer
-										key={this.state.key}
-										isPlaying={this.state.isVideoClicked}
-										duration={20}
-										colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-										onComplete={() => [true, 0]}
-										strokeWidth={8}
-										size={75}>
-										{this.renderVideoTime}
-									</CountdownCircleTimer>
-								</div>
 							</div>
 						</div>
 					</div>
@@ -314,8 +322,8 @@ class Actor extends Component {
 injectReducer(store, VIDEO, video);
 
 Actor.propTypes = {
-    history: PropTypes.object,
-    user: PropTypes.object
+	history: PropTypes.object,
+	user: PropTypes.object
 };
 
 export default withRouter(User(Actor));
