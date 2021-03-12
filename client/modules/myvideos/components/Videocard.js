dd55218276f21dbb04398f79a7a9807d51015bd6
nishant <palln6935@gmail.com>
2021-03-12 14:44:10 +0530

    Remove add to favourites

--- client/modules/myvideos/components/Videocard.js
+++ client/modules/myvideos/components/Videocard.js
@@ -11,7 +11,7 @@ class Videocard extends Component {
 		super(props);
 
 		this.state = {
-			id:this.props.id,
+			id: this.props.id,
 			title: this.props.title,
 			description: this.props.description,
 			url: this.props.url,
@@ -23,16 +23,16 @@ class Videocard extends Component {
 	render() {
 		const { id, index, title, description, url, timeCreated } = this.props;
 		// your link creation
-		const newTo = { 
-		  pathname: "/video", 
-		  param1: this.props.id 
+		const newTo = {
+			pathname: "/video",
+			param1: this.props.id
 		};
 		// link to the "location"
 		// see (https://reacttraining.com/react-router/web/api/location)
-		
+
 		return (
 			// your route setup
-			
+
 			<div className="myvideo-card-item">
 				<div className="myvideo-card">
 					<div className="myvideo-card-menu">
@@ -41,7 +41,7 @@ class Videocard extends Component {
 								<i className="fa fa-bars"></i>
 							</Dropdown.Toggle>
 							<Dropdown.Menu>
-								<Dropdown.Item><Link to={newTo}>Edit</Link></Dropdown.Item>								
+								<Dropdown.Item><Link to={newTo}>Edit</Link></Dropdown.Item>
 								<Dropdown.Item href="" onClick={() => this.props.deleteHandler(index)}>Delete</Dropdown.Item>
 								<Dropdown.Item href="" onClick={() => this.props.duplicateHandler(index)}>Duplicate</Dropdown.Item>
 							</Dropdown.Menu>
@@ -55,16 +55,16 @@ class Videocard extends Component {
 				<div className="myvideo-time">
 					<p>
 						{timeCreated ? timeCreated : "NA"}
-						<div id="myvideo-footer-icons">
+						{/* <div id="myvideo-footer-icons">
 							<label className="btn btn-sm">
 								<AiIcons.AiOutlineStar size={20} />
 							</label>
 							<span className="tooltiptext">Add to favourites</span>
-						</div>
+						</div> */}
 						<div id="myvideo-footer-icons">
 							<label
 								className="btn btn-sm"
-								onClick={() => navigator.clipboard.writeText( url )}>
+								onClick={() => navigator.clipboard.writeText(url)}>
 								<AiIcons.AiOutlineLink size={20} />
 							</label>
 							<span className="tooltiptext">Copy link</span>
