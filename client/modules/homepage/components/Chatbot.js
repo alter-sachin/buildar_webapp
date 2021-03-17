1709ef6af41e803b66a0cbe739e1ebaf5214ad47
nishant <palln6935@gmail.com>
2021-03-17 20:28:54 +0530

    Fix structure of chatbot

--- client/modules/homepage/components/Chatbot.js
+++ client/modules/homepage/components/Chatbot.js
@@ -60,51 +60,52 @@ class ChatBot extends Component {
         return (
             <div id="main">
                 <div id="chat-bot" className={this.state.transitionClass}>
-                        <ReactPlayer
-                            id="video-bot"
-                            className="videoInsert"
-                            playing={isHeadClicked}
-                            controlsList="nodownload"
-                            width="160px"
-                            height="90px"
-                            // onContextMenu={(e) => e.preventDefault()}
-                           /* style={{width: "160px",height:"90px"}}*/
-                            url={url}
-                        />
-                        {/* </div> */}
-                        <div className="overlay" >
-                            <div id="overlay-intro">
-                                <Button block variant="primary" onClick={this.changeToArticles}>Show me your best articles</Button>
-                                <Button block variant="primary" onClick={this.changeToNewsLetter}>Subscribe to my newsletter</Button>
-                                <Button block variant="primary" onClick={this.changeToTwitter}>Follow me on Twitter</Button>
-                            </div>
-                            <div id="overlay-articles">
-                                <a type="button" className="btn btn-primary btn-block" href="#">Fake News</a>
-                                <a type="button" className="btn btn-primary btn-block" href="#">How to meditate</a>
-                                <a type="button" className="btn btn-primary btn-block" href="#">Selling Startup</a>
-                            </div>
-                            <div id="overlay-email">
-                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
-                            </div>
-                            <div id="overlay-twitter">
-                                <button type="button" className="btn" href="#twitter">
-                                    <p>Here's my Twitter  </p>
-                                    <i className="fa fa-twitter fa-10x" aria-hidden="true">  </i>
-                                </button>
-                            </div>
+                    <ReactPlayer
+                        id="video-bot"
+                        className="videoInsert"
+                        playing={isHeadClicked}
+                        controlsList="nodownload"
+                        width="320px"
+                        // height="180px"
+                        // onContextMenu={(e) => e.preventDefault()}
+                        /* style={{width: "160px",height:"90px"}}*/
+                        url={url}
+                    />
+                    {/* </div> */}
+                    <div className="overlay" >
+                        <div id="overlay-intro">
+                            <Button block variant="primary" onClick={this.changeToArticles}>Show me your best articles</Button>
+                            <Button block variant="primary" onClick={this.changeToNewsLetter}>Subscribe to my newsletter</Button>
+                            <Button block variant="primary" onClick={this.changeToTwitter}>Follow me on Twitter</Button>
+                        </div>
+                        <div id="overlay-articles">
+                            <a type="button" className="btn btn-primary " href="#">Fake News</a>
+                            <a type="button" className="btn btn-primary " href="#">How to meditate</a>
+                            <a type="button" className="btn btn-primary " href="#">Selling Startup</a>
+                        </div>
+                        <div id="overlay-email">
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+                        </div>
+                        <div id="overlay-twitter">
+                            <button type="button" className="btn" href="#twitter">
+                                <p>Here's my Twitter  </p>
+                                <i className="fa fa-twitter fa-10x" aria-hidden="true">  </i>
+                            </button>
+                        </div>
                     </div>
                 </div>
                 <div id="chathead" className="chatbot-head">
                     <button type="button" className="btn" onClick={this.showBot}>
-                       <ReactPlayer
+                        <ReactPlayer
                             id="video-bot"
                             className="videoInsert"
-                            playing={true}
+                            playing
+                            muted
                             height="128px"
                             width="128px"
                             controlsList="nodownload"
                             // onContextMenu={(e) => e.preventDefault()}
-                            url={url}
+                            url="https://buildar.in/vid/1615800618.4738576.mp4"
                         />
                     </button>
                 </div>
